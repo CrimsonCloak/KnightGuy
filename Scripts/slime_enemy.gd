6272ab11091fbc112d5f3045076372e8bcddb02f
CrimsonCloak <alexander.veldeman.999@gmail.com>
2024-06-14 10:55:05 +0200

    Add slime enemy, add one to map

---
+++ Scripts/slime_enemy.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+const SPEED = 30
+
+var direction = 1
+@onready var raycast_left = $RaycastLeft
+@onready var raycast_right = $RaycastRight
+@onready var animated_sprite = $AnimatedSprite2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# When hits a wall on the right side, turn direction to left and flip sprite
+	if raycast_right.is_colliding():
+			direction = -1
+			animated_sprite.flip_h = true
+# When hits a wall on the left side, turn direction to right and flip sprite			
+	if raycast_left.is_colliding():
+			direction = 1	
+			animated_sprite.flip_h = false
+	position.x += direction * SPEED * delta
+
+
