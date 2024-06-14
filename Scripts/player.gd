9bfb7617c942f195bbe69ae529fbb00c11a6ebbc
CrimsonCloak <alexander.veldeman.999@gmail.com>
2024-06-14 11:07:57 +0200

    Add spriteflip for horizontal movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,8 +4,10 @@ extends CharacterBody2D
 const SPEED = 100.0
 const JUMP_VELOCITY = -250.0
 
+@onready var animated_sprite = $AnimatedSprite2D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -16,9 +18,19 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+# Movement
 	var direction = Input.get_axis("Move_left", "Move_right")
+	
+	# Sprite flip
+	# One-liner but defaults to looking to one specific direction
+	#animated_sprite.flip_h = true if (direction < 0) else false
+	
+	# Longer script but doesn't default to any sprite direction, only latest
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction <0:
+		animated_sprite.flip_h = true	
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
