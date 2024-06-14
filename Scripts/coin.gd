8d2cf22ded3b350645726173b7b8f9ff04b0f8b8
CrimsonCloak <alexander.veldeman.999@gmail.com>
2024-06-14 12:02:03 +0200

    Add coin collection sounds and behavior using animation

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
 @onready var game_manager = %GameManager
+@onready var animation_player = $AnimationPlayer
 
 # We will use so-called 'signals' (~ triggers) that are built-in for Godot. You can see these in the Godot-editor under the node's signals tab, next to the inspector.
 func _on_body_entered(body):
 	game_manager.add_coin()
-	queue_free()
+	animation_player.play("Collect")
