bef21cdc25dd3dee0705c6eaaa6c859186a8409d
CrimsonCloak <alexander.veldeman.999@gmail.com>
2024-06-15 12:30:22 +0200

    Add buttons with debug output

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -4,6 +4,6 @@ extends Area2D
 @onready var animation_player = $AnimationPlayer
 
 # We will use so-called 'signals' (~ triggers) that are built-in for Godot. You can see these in the Godot-editor under the node's signals tab, next to the inspector.
-func _on_body_entered(body):
+func _on_body_entered(_body):
 	game_manager.add_coin()
 	animation_player.play("Collect")
