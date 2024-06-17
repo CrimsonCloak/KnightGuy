2c1f0b9468a7b5d0789d9bd2090003aa61998731
CrimsonCloak <alexander.veldeman.999@gmail.com>
2024-06-17 10:41:18 +0200

    Primitive coin counter with icon

--- Scenes/CoinLayer.gd
+++ Scenes/CoinLayer.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
-@onready var coin_label = $CoinLabel
+@onready var coin_icon = $CoinCounterContainer/CoinIcon
+@onready var coin_label = $CoinCounterContainer/CoinLabel
 
 func update_coin_count(coin_count):
-	coin_label.text = "Coins: %d" % coin_count
+	coin_label.text = ": %d" % coin_count
