d2b69b1a92a304653fcd03e27cd8a2265cf7ac5a
CrimsonCloak <alexander.veldeman.999@gmail.com>
2024-06-17 14:43:37 +0200

    Revert "Test quit button implementation for HTML5"
    This reverts commit b73a0f256ad86483da93cf488babb418debea988.

--- Scripts/Quit_button.gd
+++ Scripts/Quit_button.gd
@@ -1,8 +1,4 @@
 extends Button
 
 func _pressed():
-	if OS.get_name() == "HTML5":
-		var js_script = "window.close();"
-		JavaScriptBridge.eval(js_script)
-	else:
-		get_tree().quit()
+	get_tree().quit()
