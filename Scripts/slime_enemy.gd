99dbf9246e73ff058c59cac481b721aa4d4e48f2
CrimsonCloak <alexander.veldeman.999@gmail.com>
2024-06-17 17:30:36 +0200

    Picking up sword makes slimes no longer kill you

--- Scripts/slime_enemy.gd
+++ Scripts/slime_enemy.gd
@@ -6,9 +6,22 @@ var direction = 1
 @onready var raycast_left = $RaycastLeft
 @onready var raycast_right = $RaycastRight
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var game_manager = %GameManager
+@onready var killzone = $Killzone
+
+var killable = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# Killable
+	if game_manager.swordCollected == true and killable == false:
+		killable = true
+		killzone.queue_free()
+		
+		
+	
+	
+	# Walking
 	# When hits a wall on the right side, turn direction to left and flip sprite
 	if raycast_right.is_colliding():
 			direction = -1
